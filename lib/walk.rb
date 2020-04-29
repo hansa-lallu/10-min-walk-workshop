6b5bca919d10bd0df729d1c2c37885b994eaa17c
hansa-lallu <hansalallu@gmail.com>
2020-04-29 13:33:47 +0100

    returns false if number of E does not match number of W

--- lib/walk.rb
+++ lib/walk.rb
@@ -1,6 +1,8 @@
 class Walk 
   def valid_walk(walk)
     return false if walk.count('n') != walk.count('s')
+    return false if walk.count('w') != walk.count('e')
     return false if walk.length != 10 
+    return true
   end
 end
--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -11,5 +11,10 @@ describe Walk do
       walk = ['s', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']
       expect(subject.valid_walk(walk)).to eq(false)
     end 
+
+    it 'returns false if s does not equal e' do
+      walk = ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']
+      expect(subject.valid_walk(walk)).to eq(false)
+    end
   end
 end
